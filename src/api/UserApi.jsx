f8a06d5d6133a026406b2b1bb516f60841547470
amanthajayathilake <amanthajayathilake@gmail.com>
2022-08-17 02:18:31 +0530

    Edit User UI

--- src/api/UserApi.jsx
+++ src/api/UserApi.jsx
@@ -7,3 +7,4 @@ export const loginUser = (authObj) => axios.post(USER_API_BASE_URL + '/login', a
 export const registerUser = (userObj) => axios.post(USER_API_BASE_URL + '/register', userObj);
 export const findUsers = (queryParams) => axios.get(USER_API_BASE_URL + `?${queryParams}`);
 export const fetchUser = (id, payload) => axios.get(USER_API_BASE_URL + `/${id}`, payload);
+export const updateUser = (id, userObj) => axios.put(USER_API_BASE_URL + `/${id}`, userObj);
