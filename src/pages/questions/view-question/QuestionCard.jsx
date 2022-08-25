ac2f960eb28cfeee05d04380206fd9377c1a85f6
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-08-25 07:31:08 +0530

    Style changes

--- src/pages/questions/view-question/QuestionCard.jsx
+++ src/pages/questions/view-question/QuestionCard.jsx
@@ -13,7 +13,7 @@ export default function QuestionCard({ title, createdAt, createdBy, onEdit, imag
                 className=""
             >
                 <div className="flex justify-between ...">
-                    <p className="text-slate-400 text-sm">@{createdBy.username} &nbsp; {timeSince(new Date(createdAt))} ago </p>
+                    <p className="text-base font-medium text-gray-600">@{createdBy.username} &nbsp; {timeSince(new Date(createdAt))} ago </p>
                     {userId == createdBy._id &&
                         <div className="text-3xl flex justify-between ...">
                             <div className="pr-5 cursor-pointer" onClick={onEdit}><BsPencilSquare /></div>
@@ -25,7 +25,7 @@ export default function QuestionCard({ title, createdAt, createdBy, onEdit, imag
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {title}
                 </h5>
-                <p className="text-slate-400 text-sm">
+                <p className="text-base font-medium text-gray-400">
                     {tags.map(tag => {
                         return (
                             <>
@@ -35,7 +35,7 @@ export default function QuestionCard({ title, createdAt, createdBy, onEdit, imag
                     })}
                 </p>
                 <div className="bg-cover">
-                    <img src={imageUrl} />
+                    <img className="rounded-lg" src={imageUrl} />
                 </div>
                 <div>
                     <p className="text-slate-600 text-lg m-3">{description}</p>
