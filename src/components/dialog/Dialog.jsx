e646be4d03eda49fd640dbef6a23749c6855bd75
amanthajayathilake <amanthajayathilake@gmail.com>
2022-08-30 02:38:01 +0530

    dialog shadow

--- src/components/dialog/Dialog.jsx
+++ src/components/dialog/Dialog.jsx
@@ -10,7 +10,7 @@ export default function Dialog(props) {
     <DialogContext.Provider value={onClose}>
       <div id="defaultModal" tabIndex={-1} className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex" aria-modal="true" role="dialog">
         <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="relative bg-white rounded-lg shadow-2xl drop-shadow-2xl dark:bg-gray-700">
             {props.children}
           </div>
         </div>
