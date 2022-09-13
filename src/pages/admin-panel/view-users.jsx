2c75dfb87d51cedfc7b31ce68821c234fe46bcb7
amanthajayathilake <amanthajayathilake@gmail.com>
2022-09-13 23:26:59 +0530

    user API loading animation

--- src/pages/admin-panel/view-users.jsx
+++ src/pages/admin-panel/view-users.jsx
@@ -5,10 +5,13 @@ import Table from '../../components/Table/Table';
 import { findUsers } from '../../api/User/userApi';
 import Editprofile from '../../components/Profile/EditProfile';
 import Dialog from '../../components/dialog/Dialog';
+import { applyToast } from '../../components/toast-message/toast';
 
 export default function ViewAllUsers() {
     const [user, setUser] = React.useState([]);
     const [users, setUsers] = React.useState([]);
+    const [apiResponseWaiting, setApiResponseWaiting] = React.useState(false);
+
     const pageSize = 5;
     const [paginatedOrders, setPaginatedOrders] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
@@ -26,6 +29,7 @@ export default function ViewAllUsers() {
     }, []);
     
     const getUsers = () => {
+        setApiResponseWaiting(true);
         findUsers()
           .then(res => {
             if(res.data.isSuccessful) {
@@ -33,6 +37,9 @@ export default function ViewAllUsers() {
                 console.log(userData)
                 setUsers(userData);
                 setPaginatedOrders(_(userData).slice(0).take(pageSize).value());
+                userData? setTimeout(function(){
+                    setApiResponseWaiting(false);
+                }, 500) : setApiResponseWaiting(false) && applyToast('Fetching failed. Try again!', 'error');
             } else {
                 console.error("error");
             }
@@ -96,50 +103,46 @@ export default function ViewAllUsers() {
                             head={
                                 <>
                                     <tr>
-                                        <th scope="col" className="py-3 px-6">
-                                            #
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            User ID
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            User Name
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            Email
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            Contact
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            User Role
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            Actions
-                                        </th>
+                                        <th scope="col" className="py-3 px-6">#</th>
+                                        <th scope="col" className="py-3 px-6">User ID</th>
+                                        <th scope="col" className="py-3 px-6">User Name</th>
+                                        <th scope="col" className="py-3 px-6">Email</th>
+                                        <th scope="col" className="py-3 px-6">Contact</th>
+                                        <th scope="col" className="py-3 px-6">User Role</th>
+                                        <th scope="col" className="py-3 px-6">Actions</th>
                                     </tr>
                                 </>
                             }
                             body={
-                                <>
-                                    {paginatedOrders.map((user) =>(
-                                        <tr className='self-center'>
-                                            <td className='py-4 px-6'>
-                                                <img src={user?.pic} className="rounded-full w-10 shadow-lg" alt="Avatar" />
-                                            </td>
-                                            <td className='py-4 px-6'>{user?._id?.substring(0, 6) || '-'}</td>
-                                            <td className='py-4 px-6'>{(user?.firstName + " " + user?.lastName)}</td>
-                                            <td className='py-4 px-6'>{user?.email}</td>
-                                            <td className='py-4 px-6'>{user?.contactNo}</td>
-                                            <td className='py-4 px-6'>{user?.role}</td>
-                                            <td className='py-4 px-6'>
-                                                <button className="" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Profile" onClick={() => setEditingUser(user)}>
-                                                    <img src="images/edit.jpg" className="rounded-full w-6 h-6 shadow-lg" alt="Edit" user={user}/>
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </> 
+                                apiResponseWaiting ?
+                                    <>
+                                        <center>
+                                            <div className="flex justify-center items-center">
+                                                <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-red-800" role="status">
+                                                    <span className="visually-hidden">Loading...</span>
+                                                </div>
+                                            </div>
+                                        </center>
+                                    </> : 
+                                    <>
+                                        {paginatedOrders.map((user) =>(
+                                            <tr className='self-center'>
+                                                <td className='py-4 px-6'>
+                                                    <img src={user?.pic} className="rounded-full w-10 shadow-lg" alt="Avatar" />
+                                                </td>
+                                                <td className='py-4 px-6'>{user?._id?.substring(0, 6) || '-'}</td>
+                                                <td className='py-4 px-6'>{(user?.firstName + " " + user?.lastName)}</td>
+                                                <td className='py-4 px-6'>{user?.email}</td>
+                                                <td className='py-4 px-6'>{user?.contactNo}</td>
+                                                <td className='py-4 px-6'>{user?.role}</td>
+                                                <td className='py-4 px-6'>
+                                                    <button className="" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Profile" onClick={() => setEditingUser(user)}>
+                                                        <img src="images/edit.jpg" className="rounded-full w-6 h-6 shadow-lg" alt="Edit" user={user}/>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </>
                             }
                             currentPage={currentPage}
                             pageCount={pageCount}
