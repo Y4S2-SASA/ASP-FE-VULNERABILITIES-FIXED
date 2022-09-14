c082539e52b29c7ba7abee1c3811493ede253ff1
ShivaaniDushya <it20006884@my.sliit.lk>
2022-09-14 22:13:34 +0530

    Search sorting done

--- src/pages/items/index.jsx
+++ src/pages/items/index.jsx
@@ -9,7 +9,6 @@ import { BsPencilSquare } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { applyToast } from '../../components/toast-message/toast';
 import { useNavigate } from "react-router-dom";
-import SearchBox from "../../components/search-box/SearchBox";
 import DeleteItem from "./DeleteItem";
 import Dialog from '../../components/DialogComponent/Dialog'
 
@@ -18,6 +17,8 @@ const Items = () => {
   const loggedInUser = useContext(AuthContext);
   const { userId, role } = loggedInUser;
   const navigate = useNavigate();
+  const [search, setSearch] = useState("");
+  const [sortStatus, setSortStatus] = useState("ASC")
 
   const initialState = [
     {
@@ -42,6 +43,22 @@ const Items = () => {
   }
 
   const [inputs, setInputs] = useState(initialInput);
+
+  //Sort Ascending function
+  const sortAsc = () => {
+    let sortedItems = item.sort((a, b) => a.createdAt.localeCompare(b.createdAt));
+    setItem(sortedItems)
+    setSortStatus('ASC')
+    console.log("I am sort Asc");
+  }
+
+  //Sort Descending function
+  const sortDesc = () => {
+    let sortedItems = item.sort((a, b) => b.createdAt.localeCompare(a.createdAt));
+    setItem(sortedItems)
+    setSortStatus('DESC')
+    console.log("I am sort Desc");
+  }
 
   useEffect(() => {
     itemRequest.getItems().then((res) => {
@@ -105,7 +122,47 @@ const Items = () => {
 
             <div className="flex justify-between py-2">
               <div>
-                <SearchBox handleSearch={handleSearch} />
+                {/* <SearchBox handleSearch={handleSearch} /> */}
+
+                <div className="flex">
+                  <div className="dropdown relative">
+                    <button id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">
+                      {
+                        sortStatus === 'ASC' ?
+                          'Newest'
+                          :
+                          sortStatus === 'DESC' ?
+                            'Oldest'
+                            :
+                            'Filter'
+                      }
+                      <svg aria-hidden="true" className="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clipRule="evenodd" />
+                      </svg>
+                    </button>
+                    <ul className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none left-auto right-0" aria-labelledby="dropdownMenuButton1">
+                      <li>
+                        <button type="button" className="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          onClick={sortAsc}>Newest</button>
+                      </li>
+                      <li>
+                        <button type="button" className="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          onClick={sortDesc}>Oldest</button>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="relative w-full">
+                    <input type="search"
+                      id="search-dropdown"
+                      className="block p-2.5 lg:w-96 z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                      placeholder="Search..."
+                      value={search} onChange={(e) => { setSearch(e.target.value) }} />
+                  </div>
+                </div>
+
+
               </div>
               <div>
                 <Button><div className="text-xl"><a href="/createitem">Create your Item</a></div> </Button>
