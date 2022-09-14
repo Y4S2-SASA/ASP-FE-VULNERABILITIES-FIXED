f33332205e6b5a271f560aee33e1e2070c3969e8
Dulshan Perera <dulshanperera55@gmail.com>
2022-09-14 19:45:18 +0530

    Implemented search functionality to ReservationRequests component

--- src/pages/reservations/ReservationRequests.jsx
+++ src/pages/reservations/ReservationRequests.jsx
@@ -16,6 +16,8 @@ export default function ReservationRequests() {
     const [buyer, setBuyer] = useState({})
     const [item, setItem] = useState({})
     const [seller, setSeller] = useState({})
+    const [search, setSearch] = useState("");
+    const [sortStatus, setSortStatus] = useState("ASC")
 
     const getOrders = () =>{
         orderRequest.getOrderRequests(sellerId) 
@@ -36,6 +38,22 @@ export default function ReservationRequests() {
             setItem(response.data.data.item);
             setSeller(response.data.data.item.createdBy);
         })
+    }
+
+    //Sort Ascending function
+    const sortAsc = () =>{
+        let sortedOrders = orders.sort((a, b) => a.createdAt.localeCompare(b.createdAt));
+        setOrders(sortedOrders)
+        setPaginatedOrders(_(orders).slice(0).take(pageSize).value());
+        setSortStatus('ASC')
+    }
+    
+    //Sort Decending function
+    const sortDesc = () =>{
+        let sortedOrders = orders.sort((a, b) => b.createdAt.localeCompare(a.createdAt));
+        setOrders(sortedOrders)
+        setPaginatedOrders(_(orders).slice(0).take(pageSize).value());
+        setSortStatus('DESC')
     }
 
     useEffect(() =>{
@@ -72,7 +90,15 @@ export default function ReservationRequests() {
                             <div className="flex">
                                 <div className="dropdown relative">
                                     <button id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">
-                                        Filter
+                                        {
+                                                sortStatus === 'ASC' ?
+                                                    'Newest'
+                                                :
+                                                sortStatus === 'DESC' ?
+                                                    'Oldest'
+                                                :
+                                                    'Filter'
+                                        }
                                         <svg aria-hidden="true" className="ml-1 w-4 h-4"  fill="currentColor"  viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" 
                                                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" 
@@ -81,19 +107,22 @@ export default function ReservationRequests() {
                                     </button>
                                     <ul className="dropdown-menu min-w-max absolute hidden bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none left-auto right-0" aria-labelledby="dropdownMenuButton1">
                                         <li>
-                                            <button type="button" className="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Ascending</button>
+                                            <button type="button" className="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                onClick={sortAsc}>Newest</button>
                                         </li>
                                         <li>
-                                            <button type="button" className="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Descending</button>
+                                            <button type="button" className="inline-flex py-2 px-4 w-full hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                onClick={sortDesc}>Oldest</button>
                                         </li>
                                     </ul>
                                 </div>
                                 <div className="relative w-full">
-                                    <input type="search" 
-                                        id="search-dropdown" 
-                                        className="block p-2.5 lg:w-96 z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" 
-                                        placeholder="Search..."/>
-                                </div>
+                                <input type="search" 
+                                    id="search-dropdown" 
+                                    className="block p-2.5 lg:w-96 z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" 
+                                    placeholder="Search..."
+                                    value={search} onChange={(e)=>{ setSearch(e.target.value)}}/>
+                            </div>
                             </div>
                             </form><br/>
                         </div>
@@ -127,7 +156,13 @@ export default function ReservationRequests() {
                             body={
                                 <>
                                 {
-                                    paginatedOrders.map((row) =>(
+                                    paginatedOrders.filter((row)=>{
+                                        if(search === ""){
+                                            return row
+                                        }else if(row.orderId.toLowerCase().includes(search.toLowerCase())){
+                                            return row
+                                        }
+                                        }).map((row) =>(
                                         <tr className='self-center'>
                                             <td className='py-4 px-6'>{row.orderId}</td>
                                             <td className='py-4 px-6'>{row.item.name}</td>
