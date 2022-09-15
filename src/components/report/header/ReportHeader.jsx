57dba03fd57f4b01d6809b49abaee7aa402548fb
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-09-15 08:37:48 +0530

    date selection function created

--- src/components/report/header/ReportHeader.jsx
+++ src/components/report/header/ReportHeader.jsx
@@ -1,28 +1,30 @@
-import Button from '../../components/buttons/Buttons';
+
+import Button from '../../buttons/Buttons';
 import './Header.css';
 
-export default function Header() {
+export default function ReportHeader(props) {
+
     return (
         <div className="report-header bg-gray-300">
             <div className="header-box">
                 <span>Start Date</span>
                 <br />
-                <input type="date" className="datepicker" />
+                <input type="date" id="startDate" className="datepicker" />
             </div>
             <div className="header-box">
                 <span>End Date</span>
                 <br />
-                <input type="date" className="datepicker" />
+                <input type="date" id="endDate" className="datepicker" />
             </div>
             <div className="header-box">
                 <div className="btn-wrapper">
-                    <Button>Generate Report</Button>
+                    <Button onClick={props.onGenerate}>Generate Report</Button>
                 </div>
             </div>
             <div className="header-box">
                 <div className="btn-wrapper">
                     <center>
-                        <Button variant="dark">Convert to PDF</Button>
+                        <Button variant="dark" onClick={() => window.print()}>Convert to PDF</Button>
                     </center>
                 </div>
             </div>
--- src/pages/question-report/QuestionReport.jsx
+++ src/pages/question-report/QuestionReport.jsx
@@ -1,129 +1,53 @@
 import NavBar from "../../components/LayoutComponents/NavBar";
-import Header from "./Header";
 import "./QuestionReport.css";
-import { Doughnut } from "react-chartjs-2";
-import Table from "../../components/Table/Table";
-import _ from 'lodash';
 import { useState } from "react";
-
-const data = {
-    labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-    ],
-    datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-    }]
-};
-
+import { getTags } from "../../api/QuestionsApi";
+import { applyToast } from "../../components/toast-message/toast";
+import DoughnutChart from "../../components/report/charts/DoughnutChart";
+import ReportHeader from "../../components/report/header/ReportHeader";
 
 export default function QuestionReport(props) {
-    const pageSize = 5;
-    const [paginatedOrders, setPaginatedOrders] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const pageCount = 0;
-    const pages = _.range(1, pageCount + 1)
+    const [tagsLabels, setTagsLabels] = useState([]);
+    const [tagsData, setTagsData] = useState([]);
 
-    const pagination = (pageNo) =>{
-        setCurrentPage(pageNo)
-        const startIndex = (pageNo -1) * pageSize;
-        const paginatedOrder = _([]).slice(startIndex).take(pageSize).value();
-        setPaginatedOrders(paginatedOrder);
+    const onGenerate = () => {
+        // getting selected start date and end date from date pickers in ReportHeader.jsx
+        const startDate = document.getElementById("startDate").value;
+        const endDate = document.getElementById("endDate").value;
+
+        // making labels and data empty to rerender the Dougnut chart after fetching data.
+        // Doughnut chart eke props wenas unama rerender wena widiyata eyala hadala na.
+        setTagsLabels([]);
+        setTagsData([]);
+
+        // fetching data and setting label and data values
+        getTags({ startDate, endDate })
+            .then(res => {
+                const tags = res.data.data;
+                setTagsLabels(Object.keys(tags));
+                setTagsData(Object.values(tags));
+            })
+            .catch(err => applyToast())
+
     }
 
     return (
         <>
             <NavBar />
-            <Header />
+            <ReportHeader onGenerate={onGenerate} />
+            <p>Choose start date and end date. then click on generate report</p>
+
             <div className="grid grid-cols-2 gap-2">
                 <div >
-                <Table
-                            head={
-                                <>
-                                    <tr>
-                                        <th scope="col" className="py-3 px-6">
-                                            Order ID
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            Item Name
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            Quantity
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            Price
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            Status
-                                        </th>
-                                        <th scope="col" className="py-3 px-6">
-                                            Action
-                                        </th>
-                                    </tr>
-                                </>
-                            }
-                            body={
-                                <>
-                     
-                            
-                                        <tr className='self-center'>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td>halo</td>
-                                        </tr>
-                                        <tr className='self-center'>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td>halo</td>
-                                        </tr>
-                                        <tr className='self-center'>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td>halo</td>
-                                        </tr>
-                                        <tr className='self-center'>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td>halo</td>
-                                        </tr>                  <tr className='self-center'>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td className='py-4 px-6'>halo</td>
-                                            <td>halo</td>
-                                        </tr>
-                  
-                                </> 
-                            }
-                            currentPage={currentPage}
-                            pageCount={pageCount}
-                            pages={pages}
-                            pagination={pagination}
-                        />
+
                 </div>
-                <div >
-                    <Doughnut data={data} />
+                <div>
+                    {tagsLabels && tagsData &&
+                        <DoughnutChart
+                            labels={tagsLabels}
+                            data={tagsData}
+                        />
+                    }
                 </div>
             </div>
 
