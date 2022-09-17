0997d453e41fe905ffd50157c2ca606642cf99ac
amanthajayathilake <amanthajayathilake@gmail.com>
2022-09-18 00:40:59 +0530

    users report table changes

--- src/pages/users-report/UsersReport.jsx
+++ src/pages/users-report/UsersReport.jsx
@@ -6,7 +6,7 @@ import ReportHeader from "../../components/report/header/ReportHeader";
 import { findUsers } from "../../api/User/userApi";
 import "./UsersReport.css";
 import Table from '../../components/Table/Table';
-import _ from 'lodash';
+import { data } from 'autoprefixer';
 
 export default function UsersReport(props) {
     const [tagsLabels, setTagsLabels] = useState([]);
@@ -15,18 +15,14 @@ export default function UsersReport(props) {
     const [adminRoles, setAdminRoles] = React.useState([]);
     const [clientRoles, setClientRoles] = React.useState([]);
     const [users, setUsers] = React.useState([]);
-
-    const [apiResponseWaiting, setApiResponseWaiting] = React.useState(false);
-    const [paginatedOrders, setPaginatedOrders] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const pageSize = 5;
-    const pageCount = dateFilteredData ? Math.ceil(dateFilteredData.length/pageSize) : 0;
-    const pages = _.range(1, pageCount + 1);
+    const [isDataAvailable, setIsDataAvailable] = React.useState(false);
 
     useEffect(() => {
-        if(pageCount === 1) return null; 
-    }, [pageCount]);
-
+        setIsDataAvailable(false);
+        getUsers();
+        console.log("tableData", tableData)
+    }, []);
+    
     const getUsers = () => {
         const startDate = document.getElementById("startDate").value;
         const endDate = document.getElementById("endDate").value;
@@ -34,6 +30,7 @@ export default function UsersReport(props) {
         findUsers()
           .then(res => {
             if(res.data.isSuccessful) {
+                setIsDataAvailable(true);
                 let userData = res.data.responseData;
                 console.log(userData)
                 setUsers(userData);
@@ -43,7 +40,6 @@ export default function UsersReport(props) {
                 setAdminRoles(userData.filter(e => e.role === "ADMIN"));
                 setTagsLabels(["All Users", `Registered (${startDate} - ${endDate})`, 'Clients', 'Admins']);
                 setTagsData([userData?.length, dateFilteredData.length, clientRoles.length, adminRoles.length]);
-                setPaginatedOrders(_(dateFilteredData).slice(0).take(pageSize).value());
                 applyToast('Generated Successfully!', 'success');
             } else {
                 console.error("error");
@@ -53,12 +49,24 @@ export default function UsersReport(props) {
           .catch(() => console.log("couldn't fetch"));
     };
 
-    const pagination = (pageNo) =>{
-        setCurrentPage(pageNo)
-        const startIndex = (pageNo -1) * pageSize;
-        const paginatedOrder = _(dateFilteredData).slice(startIndex).take(pageSize).value();
-        setPaginatedOrders(paginatedOrder);
-    };
+    const tableData = [
+        {
+            type: "All Users",
+            count: users?.length || 0
+        },
+        {
+            type: "Registered Users",
+            count: dateFilteredData?.length || 0
+        },
+        {
+            type: "Client Users",
+            count: clientRoles?.length || 0
+        },
+        {
+            type: "Admin Users",
+            count: adminRoles?.length || 0
+        }
+    ];
 
     return (
         <>
@@ -69,52 +77,31 @@ export default function UsersReport(props) {
             <div className="grid grid-cols-2 gap-2">
                 <div >
                 <Table
-                            head={
-                                <>
-                                    <tr>
-                                        <th scope="col" className="py-3 px-6">#</th>
-                                        <th scope="col" className="py-3 px-6">User ID</th>
-                                        <th scope="col" className="py-3 px-6">User Name</th>
-                                        <th scope="col" className="py-3 px-6">Email</th>
-                                        <th scope="col" className="py-3 px-6">Contact</th>
-                                        <th scope="col" className="py-3 px-6">User Role</th>
-                                        <th scope="col" className="py-3 px-6">Actions</th>
-                                    </tr>
-                                </>
-                            }
-                            body={
-                                apiResponseWaiting ?
-                                    <>
-                                        <center>
-                                            <div className="flex justify-center items-center">
-                                                <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-red-800" role="status">
-                                                    <span className="visually-hidden">Loading...</span>
-                                                </div>
-                                            </div>
-                                        </center>
-                                    </> : 
-                                    <>
-                                        {paginatedOrders.map((user) =>(
+                                        head={
                                             <>
-                                            <tr className='self-center' key={user}>
-                                                <td className='py-4 px-6'>
-                                                    <img src={user?.pic} className="rounded-full w-10 shadow-lg" alt="Avatar" />
-                                                </td>
-                                                <td className='py-4 px-6'>{user?._id?.substring(0, 6) || '-'}</td>
-                                                <td className='py-4 px-6'>{(user?.firstName + " " + user?.lastName)}</td>
-                                                <td className='py-4 px-6'>{user?.email}</td>
-                                                <td className='py-4 px-6'>{user?.contactNo}</td>
-                                                <td className='py-4 px-6'>{user?.role}</td>
-                                            </tr>
+                                                <tr>
+                                                    <th scope="col" className="py-3 px-6">
+                                                        Type
+                                                    </th>
+                                                    <th scope="col" className="py-3 px-6">
+                                                        Count
+                                                    </th>
+                                                </tr>
                                             </>
-                                        ))}
-                                    </>
-                            }
-                            currentPage={currentPage}
-                            pageCount={pageCount}
-                            pages={pages}
-                            pagination={pagination}
-                        />
+                                        }
+                                        body={
+                                            <>
+                                            {tableData.map((row) => (
+                                                <tr className='self-center'>
+                                                    <td className='py-4 px-6'>{row.type}</td>
+                                                    <div className="grid">
+                                                        <td className='py-4 px-6 justify-self-center'>{row.count}</td>
+                                                    </div>
+                                                </tr>
+                                            ))}
+                                            </> 
+                                        }
+                                    />
                 </div>
                 <div>
                     {tagsLabels && tagsData &&
