4ae8c42c5e66ac9be9c3ac43b5f4ee869e84fd2d
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-10-12 18:06:13 +0530

    questions report page

--- src/pages/question-report/QuestionReport.jsx
+++ src/pages/question-report/QuestionReport.jsx
@@ -1,14 +1,31 @@
 import NavBar from "../../components/LayoutComponents/NavBar";
 import "./QuestionReport.css";
 import { useState } from "react";
-import { getTags } from "../../api/QuestionsApi";
+import { getAllQuestions, getTags } from "../../api/QuestionsApi";
 import { applyToast } from "../../components/toast-message/toast";
 import DoughnutChart from "../../components/report/charts/DoughnutChart";
 import ReportHeader from "../../components/report/header/ReportHeader";
+import Table from "../../components/Table/Table";
 
 export default function QuestionReport(props) {
     const [tagsLabels, setTagsLabels] = useState([]);
     const [tagsData, setTagsData] = useState([]);
+    const [questions, setQuestions] = useState([]);
+
+    const filterQuestions = (questions, startDate, endDate) => {
+        const unsortedQuestions = []
+        questions.forEach((question) => {
+            const createdAt = question.createdAt.substr(0, question.createdAt.indexOf('T'));
+            if((new Date(startDate) < new Date(createdAt)) && (new Date(createdAt) < new Date(endDate))) {
+                unsortedQuestions.push(question)
+            }
+        });
+        return sortQuestionsByViews(questions);
+    }
+
+    const sortQuestionsByViews = (questions) => {
+        return questions.sort((a, b) => (a.numOfViews > b.numOViews ? -1 : 1));
+    }
 
     const onGenerate = () => {
         // getting selected start date and end date from date pickers in ReportHeader.jsx
@@ -31,19 +48,54 @@ export default function QuestionReport(props) {
             })
             .catch(err => applyToast())
 
+        getAllQuestions()
+            .then(res => {
+                const sortedQuestions = filterQuestions(res.data.data, startDate, endDate);
+                setQuestions(sortedQuestions);
+            })
+            .catch(() => applyToast());
     }
 
     return (
         <>
             <NavBar />
             <ReportHeader onGenerate={onGenerate} />
-            <p>Choose start date and end date. then click on generate report</p>
+            {(questions.length === 0 || tagsData === 0) && <p>Choose start date and end date. then click on generate report</p>}
 
             <div className="grid grid-cols-2 gap-2">
-                <div >
-
+                <div className="mt-5">
+                    {questions.length !== 0 && <Table
+                        head={
+                            <>
+                                <tr>
+                                    <th scope="col" className="py-3 px-6">
+                                        Thread ID
+                                    </th>
+                                    <th scope="col" className="py-3 px-6">
+                                        No Of Views
+                                    </th>
+                                    <th scope="col" className="py-3 px-6">
+                                        Posted By
+                                    </th>
+                                </tr>
+                            </>
+                        }
+                        body={
+                            <>
+                                {
+                                    questions.map((row) => (
+                                        <tr className='self-center'>
+                                            <td className='py-4 px-6'>{row._id}</td>
+                                            <td className='py-4 px-6 justify-self-center'>{row.numOfViews}</td>
+                                            <td className='py-4 px-6 justify-self-center'>{row.createdBy.username}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </>
+                        }
+                    />}
                 </div>
-                <div>
+                <div className="h-96 w-96 justify-self-center lg:mt-0 mt-7 p-2">
                     {tagsLabels && tagsData &&
                         <DoughnutChart
                             labels={tagsLabels}
